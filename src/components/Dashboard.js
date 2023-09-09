50709dbbee0a5aaf974ccf2b4179255062a80247
Oluwagbenga Ogundare <oluwagbengaogundare@gmail.com>
2023-09-08 21:10:33 -0400

    refactored my code

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-
 import classnames from "classnames";
 import Loading from "./Loading";
 import Panel from "./Panel";
@@ -10,98 +9,68 @@ import {
   getUserWithLeastUploads
 } from "helpers/selectors";
 
-const data = [
-  {
-    id: 1,
-    label: "Total Photos",
-    getValue: getTotalPhotos
-  },
-  {
-    id: 2,
-    label: "Total Topics",
-    getValue: getTotalTopics
-  },
-  {
-    id: 3,
-    label: "User with the most uploads",
-    getValue: getUserWithMostUploads
-  },
-  {
-    id: 4,
-    label: "User with the least uploads",
-    getValue: getUserWithLeastUploads
-  }
-
+const panels = [
+  { id: 1, label: "Total Photos", getValue: getTotalPhotos },
+  { id: 2, label: "Total Topics", getValue: getTotalTopics },
+  { id: 3, label: "User with the most uploads", getValue: getUserWithMostUploads },
+  { id: 4, label: "User with the least uploads", getValue: getUserWithLeastUploads }
 ];
 
-
 class Dashboard extends Component {
-  // initial state setup
+  //initial state setup
   state = {
-    loading: false,
-    focused: null,
+    loading: true,
+    focused: JSON.parse(localStorage.getItem("focused")) || null,
     photos: [],
     topics: []
   };
 
   selectPanel(id) {
-    this.setState((previousState) => ({
-      // set value of focused to null if the value of focused is currently set to a panel
-      focused: previousState.focused !== null ? null : id
-    }));
+    this.setState({ focused: this.state.focused === id ? null : id });
   }
 
-  componentDidMount() {
-    const focused = JSON.parse(localStorage.getItem("focused"));
-    if (focused) {
-      this.setState({ focused });
+  async componentDidMount() {
+    try {
+      const [photos, topics] = await Promise.all([
+        fetch("http://localhost:8001/api/photos").then(response => response.json()),
+        fetch("http://localhost:8001/api/topics").then(response => response.json())
+      ]);
+      
+      this.setState({ loading: false, photos, topics });
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      this.setState({ loading: false });
     }
-
-    const urlsPromise = [
-      "http://localhost:8001/api/photos",
-      "http://localhost:8001/api/topics",
-    ].map((url) => fetch(url).then(response => response.json()));
-
-    Promise.all(urlsPromise)
-      .then(([photos, topics]) => {
-        this.setState({
-          loading: false,
-          photos: photos,
-          topics: topics
-        });
-      });
   }
-  
 
-  componentDidUpdate(previousState) {
-    if (previousState.focused !== this.state.focused) {
+  componentDidUpdate(_, prevState) {
+    if (prevState.focused !== this.state.focused) {
       localStorage.setItem("focused", JSON.stringify(this.state.focused));
     }
   }
 
   render() {
-    const dashboardClasses = classnames("dashboard", {
-      "dashboard--focused": this.state.focused
-     });
+    const { loading, focused } = this.state;
+    const dashboardClasses = classnames("dashboard", { "dashboard--focused": focused });
 
-    if (this.state.loading) {
+    if (loading) {
       return <Loading />;
     }
 
-    console.log(this.state);
-
     return (
       <main className={dashboardClasses}>
-        {!this.state.focused ?
-        data.map((panel)=>(
-          <Panel key={panel.id} id={panel.id} label={panel.label} value={panel.getValue(this.state)} onSelect={() => this.selectPanel(panel.id)} />
-        )) :
-        data.filter((panel) => (this.state.focused === panel.id)).map((panel)=>(
-          <Panel key={panel.id} id={panel.id} label={panel.label} value={panel.getValue(this.state)} onSelect={() => this.selectPanel(panel.id)}/>
-        ))
-        }
+        {panels.map(panel => (
+          <Panel
+            key={panel.id}
+            id={panel.id}
+            label={panel.label}
+            value={panel.getValue(this.state)}
+            onSelect={() => this.selectPanel(panel.id)}
+          />
+        ))}
       </main>
     );
   }
 }
+
 export default Dashboard;
